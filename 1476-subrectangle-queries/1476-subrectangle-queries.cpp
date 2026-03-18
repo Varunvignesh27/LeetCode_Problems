8bb9d8f8b6444d389e623ad045c7008ad805167a
Varunvignesh27 <varunvignesh006@gmail.com>
2026-03-18 20:40:13 +0530

    Time: 36 ms (6.13%), Space: 25.4 MB (61.34%) - LeetHub

--- 1476-subrectangle-queries/1476-subrectangle-queries.cpp
+++ 1476-subrectangle-queries/1476-subrectangle-queries.cpp
@@ -4,15 +4,7 @@ public:
     vector<vector<int>> mat;
 
     SubrectangleQueries(vector<vector<int>>& rectangle) {
-        int n = rectangle.size();
-        int m = rectangle[0].size();
-        mat.resize(n,vector<int>(m));
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                mat[i][j] = rectangle[i][j];
-            }
-        }
+        mat = rectangle;
     }
     
     void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
