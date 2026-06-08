05dc4fe1ea1b618f282d0fa308134bfae116d31f
Varunvignesh27 <varunvignesh006@gmail.com>
2026-06-08 14:58:59 +0530

    Time: 1492 ms (6.1%), Space: 70.8 MB (93.23%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,7 +1,35 @@
 class Solution {
 public:
+
+    int partition(vector<int>& nums,int l,int h){
+        int p = l + random() % (h-l+1);
+        swap(nums[p],nums[h]);
+
+        int pivot = nums[h];
+        int i = l-1;
+        for(int j=l;j<h;j++){
+            if(nums[j] < pivot){
+                i++;
+                swap(nums[i],nums[j]);
+            }
+        }
+        swap(nums[i+1],nums[h]);
+        return i+1;
+    }
+
+    void quicksort(vector<int>& nums,int l,int h){
+        if(l < h){
+            int pi = partition(nums,l,h);
+
+            quicksort(nums,l,pi-1);
+            quicksort(nums,pi+1,h);
+        }
+    }
+
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+
+        quicksort(nums,0,nums.size()-1);
         return nums;
+        
     }
 };
