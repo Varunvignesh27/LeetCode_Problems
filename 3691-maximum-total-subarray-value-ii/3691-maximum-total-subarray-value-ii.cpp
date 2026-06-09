4afc6a0b6f737932d01888b4a2bb40a329561659
Varunvignesh27 <varunvignesh006@gmail.com>
2026-06-09 15:50:29 +0530

    Time: 518 ms (73.55%), Space: 356.9 MB (16.53%) - LeetHub

---
+++ 3691-maximum-total-subarray-value-ii/3691-maximum-total-subarray-value-ii.cpp
@@ -0,0 +1,69 @@
+#define F first
+#define S second
+#define ll long long
+
+class Solution {
+public:
+
+    vector<vector<pair<ll,ll>>> st;
+    const int K = 25;
+
+    void build(vector<int>& nums,int n){
+        st.resize(K+1,vector<pair<ll,ll>>(n+1));
+
+        for(int i=0;i<n;i++){
+            st[0][i] = {nums[i],nums[i]};
+        }
+
+        for(int i=1;i<=K;i++){
+            for(int j=0;j + (1 << i-1) <= n;j++){
+                st[i][j] = merge(st[i-1][j],st[i-1][j + (1 << i-1)]);
+            }
+        }
+    }
+
+    pair<ll,ll> merge(pair<ll,ll>& a, pair<ll,ll>& b){
+        return {max(a.F,b.F),min(a.S,b.S)};
+    }
+
+    pair<ll,ll> rangeQuery(int l,int r){
+        pair<ll,ll> res = {-1e18,1e18};
+        for(int i = K;i >= 0;i--){
+            if(r-l+1 >= (1 << i)){
+                res = merge(res,st[i][l]);
+                l += (1 << i);
+            }
+        }
+        return res;
+    }
+
+    long long maxTotalValue(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        priority_queue<pair<ll,pair<int,int>>> pq;
+        build(nums,n);
+
+        for(int l=0;l<n;l++){
+            pair<ll,ll> pi = rangeQuery(l,n-1);
+            ll val = pi.F - pi.S;
+            pq.push({val,{l,n-1}});
+        }
+
+        long long ans = 0;
+        while(k--){
+            ans += pq.top().F;
+            pair<int,int> range = pq.top().S;
+            pq.pop();
+
+            int l = range.F;
+            int r = range.S - 1;
+            if(r >= l){
+                pair<ll,ll> pi = rangeQuery(l,r);
+                ll val = pi.F - pi.S;
+                pq.push({val,{l,r}});
+            }
+        }
+
+        return ans;
+    }
+};
