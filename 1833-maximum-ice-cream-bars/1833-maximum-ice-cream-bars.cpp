f4f0c1fad5893515b795310db3b79015921b8aad
Varunvignesh27 <varunvignesh006@gmail.com>
2026-06-22 14:48:53 +0530

    Time: 9 ms (84.35%), Space: 84.1 MB (27.55%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,12 +1,32 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
+        int n = costs.size();
+        int mx = *max_element(costs.begin(),costs.end());
+        int count[mx+1];
+        memset(count,0,sizeof(count));
         int res = 0;
-        sort(costs.begin(),costs.end());
-        for(int i : costs){
-            if(i <= coins){
+
+        for(int i : costs)
+            count[i]++;
+
+        for(int i=1;i<=mx;i++)
+            count[i] += count[i-1];
+
+        vector<int> sorted(n);
+
+        //in count[i] is represent the last index of i(value)
+
+        for(int i=0;i<n;i++){
+            int idx = count[costs[i]] - 1;
+            sorted[idx] = costs[i];
+            count[costs[i]]--;
+        }
+
+        for(int i=0;i<n;i++){
+            if(sorted[i] <= coins){
                 res++;
-                coins -= i;
+                coins -= sorted[i];
             }
         }
         return res;
