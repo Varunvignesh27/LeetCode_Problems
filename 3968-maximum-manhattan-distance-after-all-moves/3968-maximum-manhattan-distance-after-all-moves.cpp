bb0f68d836455c3509b514fe2297373cce18c309
Varunvignesh27 <varunvignesh006@gmail.com>
2026-06-24 10:24:46 +0530

    Time: 19 ms (36.45%), Space: 22.3 MB (29.01%) - LeetHub

--- 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.cpp
+++ 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.cpp
@@ -3,19 +3,13 @@ public:
     int maxDistance(string moves) {
         int x = 0,y = 0;
         int cnt = 0;
-        for(char c : moves){
-            if(c == 'U')
-                y++;
-            if(c == 'D')
-                y--;
-            if(c == 'L')
-                x--;
-            if(c == 'R')
-                x++;
-            if(c == '_'){
-                cnt++;
-            }
+        for(char &c : moves){
+            if(c == 'U') y++;
+            else if(c == 'D') y--;
+            else if(c == 'L') x--;
+            else if(c == 'R') x++;
+            else cnt++;
         }
-        return (abs(x) + abs(y))+cnt;
+        return abs(x) + abs(y) +cnt;
     }
 };
