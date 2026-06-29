c4085b3473e75f2bd1bba67b04d7e4409e1675f0
Varunvignesh27 <varunvignesh006@gmail.com>
2026-06-29 09:17:33 +0530

    Time: 4 ms (15.05%), Space: 11.7 MB (61.65%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     int numOfStrings(vector<string>& patterns, string word) {
-        int n = word.size();
         int res = 0;
-
-        for(int i=0;i<n;i++){
-            string temp = "";
-            for(int j=i;j<n;j++){
-                temp += word[j];
-                for(int k=0;k<patterns.size();k++)
-                    if(patterns[k] == temp){
-                        res++;
-                        patterns[k] = " ";
-                    }
-            }
+        for(string x : patterns){
+            if(word.find(x) != string::npos)
+                res++;
         }
-
         return res;
     }
 };
