45ab9955667a5a146316f9b132f8a518405e1576
Varun vignesh M <varunvignesh006@gmail.com>
2026-09-05 12:12:22 +0530

    Time: 34 ms (22.43%), Space: 73.4 MB (16.22%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> indegree(n+1),outdegree(n+1);
+
+		for(auto i : trust){
+			outdegree[i[0]]++;
+			indegree[i[1]]++;
+		}
+
+		for(int i=1;i<n+1;i++){
+			if(indegree[i] == n-1 && outdegree[i] == 0)
+				return i;
+		}
+
+		return -1;
+    }
+};
